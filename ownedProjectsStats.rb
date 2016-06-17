d8d50cca42e26412bb925678af4117024679984b
dcdholder <dcdholder@gmail.com>
2016-06-16 22:31:38 -0400

    Got it working

--- ownedProjectsStats.rb
+++ ownedProjectsStats.rb
@@ -1,57 +1,67 @@
 require 'rubygems'
 require 'json'
 
-puts "Enter your github username: "
+print "Enter your github username: "
 username = gets.chomp
-puts "Enter your github password: "
+print "Enter your github password: "
 password = gets.chomp
+
+puts ""
+puts "Loading your contribution statistics..."
 
 ownerUrl = "https://api.github.com/user/repos"
 authenticationString = "#{username}:#{password}"
-projectsString = `curl -i #{url} -u #{authenticationString}`
+projectsString = `curl -H "Accept: application/json" -X GET #{ownerUrl} -u #{authenticationString} --silent`
 
-projectsHash = JSON.parse(jsonString)
+projectsHash = JSON.parse(projectsString)
 projectContributions = Hash.new
 
-#TODO: needs to be tested and fixed
+totalDeletions = 0
+totalAdditions = 0
+total          = 0
+
+statsHash = Hash.new
+
 projectsHash.each do |project|
 	projectName = project["name"]
 	statsUrl = "https://api.github.com/repos/#{username}/#{projectName}/stats/contributors"
 
-	statsString = `curl -i #{statsUrl} -u #{authenticationString}`
-	projectStats = JSON.parse(contributorsString)
+	statsString = `curl -H "Accept: application/json" -X GET #{statsUrl} -u #{authenticationString} --silent`
+	projectStats = JSON.parse(statsString)
+	
+	projectStatsHash = projectStats[0]
 
-	statsHash = Hash.new
-
-	projectStats.each do |contributor|
-		if contributor["author"].equals("username")
-			statsHash["#{projectName}"] = Hash.new
-			statsHash["#{projectName}"]["additions"] = 0;
-			statsHash["#{projectName}"]["deletions"] = 0;
-			statsHash["#{projectName}"]["subtotal"] = 0;
-
-			weeklyContributions = contributor["weeks"]
-			weeklyContributions.each do |statitisticsHash|
-				statsHash["projectName"]["additions"] += statisticsHash["a"].to_i
-				statsHash["projectName"]["deletions"] += statisticsHash["d"].to_i
-			end
-			statsHash["#{projectName}"]["subtotal"] = thisProjectAdditions - thisProjectDeletions
+	if projectStatsHash["author"]["login"] == "#{username}"
+		statsHash["#{projectName}"] = {"additions" => 0, "deletions" => 0, "subtotal" => 0}
+			
+		weeklyContributions = projectStatsHash["weeks"]
+		weeklyContributions.each do |statisticsHash|
+			currentAdditions = statsHash["#{projectName}"]["additions"]
+			currentDeletions = statsHash["#{projectName}"]["deletions"]
+			statsHash["#{projectName}"]["additions"] = currentAdditions + statisticsHash["a"].to_i
+			statsHash["#{projectName}"]["deletions"] = currentDeletions + statisticsHash["d"].to_i
 		end
-
-		statsHash.each do |projectNames|
-			deletions+=statsHash["#{projectName}"]["deletions"]
-			additions+=statsHash["#{projectName}"]["additions"]
-			total+=statsHash["#{projectName}"]["subtotal"]
-		end
-
-		statsHash["total"] = total
+		subtotal = statsHash["#{projectName}"]["additions"] - statsHash["#{projectName}"]["deletions"]
+		statsHash["#{projectName}"]["subtotal"] = subtotal
 	end
-
-	statsHash.each do |stats|
-		#TODO: here's where we output everything
-	end
+	
+	totalDeletions = totalDeletions + statsHash["#{projectName}"]["deletions"]
+	totalAdditions = totalAdditions + statsHash["#{projectName}"]["additions"]
+	total          = total          + statsHash["#{projectName}"]["subtotal"]
 end
 
+statsHash["all projects"] = {"additions" => "#{totalAdditions}", "deletions" => "#{totalDeletions}", "subtotal" => "#{total}"}
+statsHash.each do |projectName, stats|
+	additions  = stats["additions"]
+	deletions  = stats["deletions"]
+	difference = stats["subtotal"]
+
+	puts ""
+	puts "Stats for #{projectName}:"
+	puts "	Additions  : #{additions}"
+	puts "	Deletions  : #{deletions}"
+	puts "	Difference : #{difference}"
+end
 
 
 
